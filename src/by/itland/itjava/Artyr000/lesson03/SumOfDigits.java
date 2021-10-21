6f5a0ee8aff92db76ce1b969700730b0b101af89
Artyr <protaser70@gmail.com>
2021-10-21 21:34:55 +0300

    homework

---
+++ src/by/itland/itjava/Artyr000/lesson03/SumOfDigits.java
@@ -0,0 +1,16 @@
+package by.itland.itjava.Artyr000.lesson03;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int a = x/1000;
+        int b = x/100%10;
+        int c = x/10%10;
+        int d = x%10;
+
+    }
+}
