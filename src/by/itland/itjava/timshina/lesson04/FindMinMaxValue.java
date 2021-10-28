c1b60e5184ae4cc479c7e6019e0b9786530f025d
Anastasia_Timshina <nastatimshina@gmail.com>
2021-10-28 21:11:25 +0300

    firstcommit1

---
+++ src/by/itland/itjava/timshina/lesson04/FindMinMaxValue.java
@@ -0,0 +1,16 @@
+package by.itland.itjava.timshina.lesson04;
+
+import java.util.Scanner;
+
+public class FindMinMaxValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int min = a;
+        int max = b;
+        System.out.printf("min: %d\nmax: %d", a, b);
+    }
+}
