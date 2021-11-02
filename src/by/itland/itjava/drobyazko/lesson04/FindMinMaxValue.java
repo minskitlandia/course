3234b9d56f236b54515954721a4b2bf2354c8c33
first commit <sasha.drobyazko.17@gmail.com>
2021-11-02 08:36:41 +0300

    lesson03

---
+++ src/by/itland/itjava/drobyazko/lesson04/FindMinMaxValue.java
@@ -0,0 +1,30 @@
+package by.itland.itjava.drobyazko.lesson04;
+
+import java.util.Scanner;
+
+public class FindMinMaxValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        int min =  findMin(findMin (a,b), findMin(c,d));
+        int max =  findMax(findMax (a,b), findMax(c,d));
+        System.out.printf("min: %d\nmax: %d", min, max);
+
+    }
+    private static int findMin (int a, int b){
+        if (a<b)
+            return a;
+        else
+            return b;
+    }
+    private static int findMax (int a, int b) {
+        if (a > b)
+            return a;
+        else
+            return b;
+    }
+
+}
