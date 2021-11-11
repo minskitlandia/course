2f9d8a0adee6dc4ee992a593d3243a8f847da02b
Anastasia_Timshina <nastatimshina@gmail.com>
2021-11-11 21:18:26 +0300

    11.11

--- src/by/itland/itjava/timshina/lesson03/AgeHuman.java
+++ src/by/itland/itjava/timshina/lesson03/AgeHuman.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class AgeHuman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         int age = scanner.nextInt();
-
+        System.out.print(name + " ");
         if (age % 10 == 1 && age % 100 != 11){
             System.out.println(age+" год");
         }
         else if(age % 10==2 || age%10==3 || age%10==4){
-            System.out.println(age+" годa");
+            System.out.println(age+" года");
         }
         else{
             System.out.println(age+" лет");
---
+++ src/by/itland/itjava/timshina/lesson05/CalcNumber.java
@@ -0,0 +1,62 @@
+package by.itland.itjava.timshina.lesson05;
+
+import java.util.Scanner;
+
+public class CalcNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println("Сумма: " + sumSequence(n, k));
+        System.out.println("Произведение: " + mulSequence(n, k));
+        System.out.println(n + "! = " + factorial(n));
+        System.out.println("Сумма четных: "+ sumEven(n, k));
+        System.out.println("Сумма нечетных: " + sumOdd(n, k));
+    }
+
+    static int sumSequence(int n, int k) {
+        int sum = 0;
+        for (int i = n; i <= k; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    static int mulSequence(int n, int k) {
+        int sum = 1;
+        for (int i = n; i < k; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
+
+    static int factorial(int n) {
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            sum *= i;
+        }
+        return sum;
+    }
+
+    static int sumEven(int n, int k) {
+        int sum = 0;
+
+        for (int i = n; i <= k; i += 2) {
+            if (i % 2 != 0)
+                continue;
+            sum += i;
+        }
+        return sum;
+
+    }
+    static int sumOdd(int n, int k){
+        int sum = 0;
+
+        for (int i = n; i <= k; i ++) {
+            if (i % 2 == 0)
+                continue;
+            sum += i;
+        }
+        return sum;
+    }
+}
