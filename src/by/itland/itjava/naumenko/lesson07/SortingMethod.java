a6e89525b7bebf25f860668cf85711b8d8732566
prepod2 <minskitlandia@gmail.com>
2021-12-23 21:23:11 +0300

    lesson09.naumenko

---
+++ src/by/itland/itjava/naumenko/lesson07/SortingMethod.java
@@ -0,0 +1,14 @@
+package by.itland.itjava.naumenko.lesson07;
+
+public class SortingMethod {
+    public static long counterQuick;
+
+    public static void quickSort(int[] arrayCopy, int i, int i1) {
+    }
+
+    public static void mergeSort(int[] arrayCopy, int size) {
+    }
+
+    public static void selectionSort(int[] arrayCopy) {
+    }
+}
