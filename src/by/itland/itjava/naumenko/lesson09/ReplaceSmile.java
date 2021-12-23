656a9eb5dde25e979ad9349f7765ee254e9d76fb
prepod2 <minskitlandia@gmail.com>
2021-12-23 18:16:35 +0300

    lesson09.naumenko

---
+++ src/by/itland/itjava/naumenko/lesson09/ReplaceSmile.java
@@ -0,0 +1,12 @@
+package by.itland.itjava.naumenko.lesson09;
+
+import java.util.Scanner;
+
+public class ReplaceSmile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String res = line.replaceAll(":\\(", ":)");
+        System.out.println(res);
+    }
+}
