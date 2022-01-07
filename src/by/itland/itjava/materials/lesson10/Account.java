47bff9e62167e8a9c88fe0b6690c65473f60e160
naymc <naymchik@yandex.ru>
2022-01-08 01:41:38 +0300

    materials.lesson10

---
+++ src/by/itland/itjava/materials/lesson10/Account.java
@@ -0,0 +1,17 @@
+package by.itland.itjava.materials.lesson10;
+
+public class Account {
+    private static int id;
+    private double balance;
+
+    public Account(double balance) {
+        id++;
+        if (balance > 0) {
+            this.balance = balance;
+        }
+    }
+
+    void showInfo() {
+        System.out.printf("id: %d %nbalance: %.2f%n", id, balance);
+    }
+}
