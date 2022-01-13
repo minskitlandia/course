842eedbdddf70531d6667a4f7cb60a8a9e368011
Sasha <sasha.drobyazko.17@gmail.com>
2022-01-13 21:18:56 +0300

    add lesson8

--- src/by/itland/itjava/drobyazko/lesson10/Runner.java
+++ src/by/itland/itjava/drobyazko/lesson10/Runner.java
@@ -3,7 +3,7 @@ package by.itland.itjava.drobyazko.lesson10;
 public class Runner {
     public static void main(String[] args) {
 
-//        Rectangle rectangle = new Rectangle(new Point(0,0), new Point(3,3));
+///        Rectangle rectangle = new Rectangle(new Point(0,0), new Point(3,3));
 ////        System.out.println(rectangle.checkSquare());
 ////        rectangle.draw();
         ComplexNumber complexNumber = new ComplexNumber(5,2);
