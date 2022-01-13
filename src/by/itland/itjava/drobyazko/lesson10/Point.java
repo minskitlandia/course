31ebce18ccd4ac4d169b3d9f1c41df2173efde62
Sasha <sasha.drobyazko.17@gmail.com>
2022-01-13 21:18:44 +0300

    add lesson8

---
+++ src/by/itland/itjava/drobyazko/lesson10/Point.java
@@ -0,0 +1,32 @@
+package by.itland.itjava.drobyazko.lesson10;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return x;
+    }
+
+    public void setY(int x) {
+        this.x = x;
+    }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+
+}
