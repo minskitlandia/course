4a79617376da717ec7ce16326037a5fe93f8e4f5
Anastasia_Timshina <nastatimshina@gmail.com>
2022-01-13 21:19:20 +0300

    13.01

---
+++ src/by/itland/itjava/timshina/lesson10/Runner.java
@@ -0,0 +1,19 @@
+package by.itland.itjava.timshina.lesson10;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        /*Rectangle rectangle = new Rectangle(new Point(0, 0), new Point(3, 3));
+        System.out.println(rectangle.checkSquare());
+        rectangle.draw();*/
+
+        ComplexNumber complexNumber = new ComplexNumber(5, 2);
+        System.out.println(complexNumber);
+        System.out.println(complexNumber.abs());
+        complexNumber.updateReal(-2);
+        complexNumber.updateIm(3);
+        System.out.println(complexNumber);
+
+    }
+}
+
