282a4c177c49451a45dc150f9b3c6d0350a8eb80
prepod2 <minskitlandia@gmail.com>
2022-01-13 21:29:38 +0300

    lesson010.naumenko

--- src/by/itland/itjava/naumenko/lesson10/Runner.java
+++ src/by/itland/itjava/naumenko/lesson10/Runner.java
@@ -1,13 +1,17 @@
 package by.itland.itjava.naumenko.lesson10;
 
+<<<<<<< HEAD
 import java.time.Year;
 import java.util.Calendar;
 
+=======
+>>>>>>> f9147c2 (lesson010.naumenko)
 public class Runner {
 
     public static void main(String[] args) {
 
 
+<<<<<<< HEAD
         System.out.println(Year.now().getValue());
         System.out.println(Calendar.getInstance().get(Calendar.YEAR));
 
@@ -24,5 +28,20 @@ public class Runner {
 
 
 
+=======
+//        Point left = new Point(-5, 3);
+//        Point right = new Point(3, 4);
+//
+//        Rectangle rectangle = new Rectangle(left, right);
+//        System.out.println(rectangle.checkSquare());
+//        rectangle.draw();
+
+        ComplexNumber complexNumber = new ComplexNumber(5, 2);
+        System.out.println(complexNumber);
+        System.out.printf("%.2f\n", complexNumber.abs());
+        complexNumber.updateReal(-2);
+        complexNumber.updateIm(3);
+        System.out.println(complexNumber);
+>>>>>>> f9147c2 (lesson010.naumenko)
     }
 }
