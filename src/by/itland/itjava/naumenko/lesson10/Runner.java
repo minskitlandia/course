dbb6d45543b730526c1c05e95e30c8fcb030511a
prepod2 <minskitlandia@gmail.com>
2022-01-13 21:29:38 +0300

    lesson010.naumenko

--- src/by/itland/itjava/naumenko/lesson10/Runner.java
+++ src/by/itland/itjava/naumenko/lesson10/Runner.java
@@ -5,18 +5,18 @@ public class Runner {
     public static void main(String[] args) {
 
 
-        System.out.println(Year.now().getValue());
-        System.out.println(Calendar.getInstance().get(Calendar.YEAR));
+//        Point left = new Point(-5, 3);
+//        Point right = new Point(3, 4);
+//
+//        Rectangle rectangle = new Rectangle(left, right);
+//        System.out.println(rectangle.checkSquare());
+//        rectangle.draw();
 
-
-        Roll roll = new Roll(18, 342);
-        System.out.println(roll.percentage());
-        System.out.println(roll.sumDenominator());
-        System.out.println(roll.checkSimpleNumerator());
-        roll.reduce();
-        roll.getDenominator();
-        roll.getNumerator();
-
-        System.out.println(roll);
+        ComplexNumber complexNumber = new ComplexNumber(5, 2);
+        System.out.println(complexNumber);
+        System.out.printf("%.2f\n", complexNumber.abs());
+        complexNumber.updateReal(-2);
+        complexNumber.updateIm(3);
+        System.out.println(complexNumber);
     }
 }
