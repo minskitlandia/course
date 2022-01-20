933c5b649a4797cb48369b4b937aa6bf2fa10f01
Sasha <sasha.drobyazko.17@gmail.com>
2022-01-20 21:09:56 +0300

    add lesson8

---
+++ src/by/itland/itjava/drobyazko/lesson10/Triangle.java
@@ -0,0 +1,48 @@
+package by.itland.itjava.drobyazko.lesson10;
+
+public class Triangle {
+
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public void setA(Point a) {
+        this.a = a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public void setB(Point b) {
+        this.b = b;
+    }
+
+    public Point getC() {
+        return c;
+    }
+
+    public void setC(Point c) {
+        this.c = c;
+    }
+
+    public double perimeter() {
+        return a.distance(b) + a.distance(c) + c.distance(b);
+    }
+
+    public double square() {
+        double p = perimeter() / 2;
+        return Math.sqrt(p * (p - a.distance(b)) * (p - a.distance(c)) * (p - c.distance(b)));
+    }
+
+}
