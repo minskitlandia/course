e03f92667040316b374ae4f7d1f5c255578e475e
Anastasia_Timshina <nastatimshina@gmail.com>
2022-01-20 21:27:00 +0300

    13.01

---
+++ src/by/itland/itjava/timshina/lesson10/Triangle.java
@@ -0,0 +1,52 @@
+package by.itland.itjava.timshina.lesson10;
+
+public class Triangle {
+
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle() {
+    }
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public void setA(Point a) {
+        this.a = a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public void setB(Point b) {
+        this.b = b;
+    }
+
+    public Point getC() {
+        return c;
+    }
+
+    public void setC(Point c) {
+        this.c = c;
+    }
+
+    public double perimeter() {
+        return a.distance(b) + a.distance(c) + b.distance(c);
+
+    }
+
+    public double square() {
+        double p = perimeter() / 2;
+        return Math.sqrt(p * (p - a.distance(b)) * (p - a.distance(c)) * (p - b.distance(c)));
+
+    }
+}
