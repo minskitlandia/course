99b76719bf35e38012341ee974e89e5e2333f6a7
Anastasia <nastatimhina@gmail.com>
2022-01-24 03:21:29 +0300

    09.01

--- src/by/itland/itjava/timshina/lesson10/Runner.java
+++ src/by/itland/itjava/timshina/lesson10/Runner.java
@@ -1,5 +1,7 @@
 package by.itland.itjava.timshina.lesson10;
 
+import java.awt.*;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -17,9 +19,7 @@ public class Runner {
         /*Point a = new Point(0, 0);
         Point b = new Point(5, 0);
         Point c = new Point(2, 7);
-
         Triangle triangle = new Triangle(a, b, c);
-
         System.out.printf("%.2f%n", triangle.perimeter());
         System.out.println( triangle.square());*/
 
@@ -29,13 +29,24 @@ public class Runner {
         System.out.println(book.pricePage());
         System.out.println(book.getPrice());*/
 
-        Roll roll = new Roll(2, 10);
+        /*Roll roll = new Roll(2, 10);
         System.out.println("Выразить значение дроби в процентах: " + roll.procRoll() + "%");
         System.out.println("вычисления суммы цифр знаменателя: " + roll.sumRoll());
         System.out.println("метод определения является ли числитель простым или составным: " + roll.checkUp());
         roll.sokrRoll();
         System.out.println(roll.getUp());
-        System.out.println(roll.getDown());
+        System.out.println(roll.getDown());*/
+
+        /*Box box = new Box(3, 4, 5);
+        System.out.println(box.volumeSquare());
+        System.out.printf("Диагональ: %.2f\n", box.diagonal());
+        System.out.println(box.check());*/
+
+        /*Door door = new Door(1, 2);
+        Window window = new Window(2, 1);
+        Room room = new Room(20, 30, 5, 2);
+        System.out.println(room.square1());
+        System.out.println(room.square2());
     }
 }
 
