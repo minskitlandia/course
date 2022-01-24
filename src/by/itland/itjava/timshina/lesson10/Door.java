5d47775a68607ce98608ff1d097601bf61127f0d
Anastasia <nastatimhina@gmail.com>
2022-01-24 03:21:29 +0300

    09.01

---
+++ src/by/itland/itjava/timshina/lesson10/Door.java
@@ -0,0 +1,30 @@
+package by.itland.itjava.timshina.lesson10;
+
+/*public class Door {
+    private int h;
+    private int w;
+
+    public Door() {
+    }
+
+    public Door(int h, int w) {
+        this.h = h;
+        this.w = w;
+    }
+
+    public static int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public static int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+}
