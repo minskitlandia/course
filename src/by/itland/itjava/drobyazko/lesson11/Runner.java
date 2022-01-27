4d33085e37202ef96b4bac97f18a0c673dca77bc
Sasha <sasha.drobyazko.17@gmail.com>
2022-01-27 21:08:58 +0300

    add lesson8

---
+++ src/by/itland/itjava/drobyazko/lesson11/Runner.java
@@ -0,0 +1,10 @@
+package by.itland.itjava.drobyazko.lesson11;
+
+public class Runner {
+    public static void main(String[] args) {
+        Chair taburet = new Chair ("табуретка",60,40.5,4,50,false);
+        Chair stul = new Chair ("стул",60,40.5,4,50,false);
+
+        //дописать скрин
+    }
+}
