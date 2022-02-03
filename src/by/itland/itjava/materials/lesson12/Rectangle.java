1afad10130e01b3495a2758325d9cb4cea8d68b2
naymc <naymchik@yandex.ru>
2022-02-03 14:06:59 +0300

    materials.lesson12

---
+++ src/by/itland/itjava/materials/lesson12/Rectangle.java
@@ -0,0 +1,9 @@
+package by.itland.itjava.materials.lesson12;
+
+public class Rectangle extends Geom {
+
+    @Override
+    void draw() {
+        System.out.println("Рисуем прямоугольник");
+    }
+}
---
+++ src/by/itland/itjava/materials/lesson12/Triangle.java
@@ -0,0 +1,8 @@
+package by.itland.itjava.materials.lesson12;
+
+public class Triangle extends Geom {
+    @Override
+    void draw() {
+        System.out.println("Рисуем Треугольник");
+    }
+}
