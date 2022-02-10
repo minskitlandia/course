aa22047e99c76c8498cbec53334863881c3e7fb2
leksi41 <lexagang123@gmail.com>
2022-02-10 21:02:31 +0300

    lesson13

---
+++ src/by/itland/itjava/bulatsky/lesson13/Scalar.java
@@ -0,0 +1,27 @@
+package by.itland.itjava.bulatsky.lesson13;
+
+public class Scalar extends Var {
+
+    private double value;
+
+    public Scalar(double value){
+        this.value = value;
+    }
+
+
+    public Scalar(String value){
+        this.value = Double.parseDouble(value);
+    }
+
+    public Scalar(Scalar scalar){
+        this.value = scalar.value;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.value);
+    }
+}
+
+
