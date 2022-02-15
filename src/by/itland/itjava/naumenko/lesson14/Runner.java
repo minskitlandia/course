00de53eff3df85c5496bc9d06fdfcfebf986f4af
prepod2 <minskitlandia@gmail.com>
2022-02-15 21:16:05 +0300

    lesson14.naumenko

---
+++ src/by/itland/itjava/naumenko/lesson14/Runner.java
@@ -0,0 +1,35 @@
+package by.itland.itjava.naumenko.lesson14;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Var scalar = new Scalar("5");
+        Var scalar2 = new Scalar("5");
+        Var vector = new Vector("{1, 2, 3}");
+        Var vector2 = new Vector("{1, 2, 3}");
+        //операции со Scalar
+        System.out.println(scalar.add(scalar2));
+        System.out.println(scalar.mul(scalar2));
+        System.out.println(scalar.sub(scalar2));
+        System.out.println(scalar.div(scalar2));
+
+        //Операции с Vector
+        System.out.println(vector.add(scalar2));
+        System.out.println(vector.mul(scalar2));
+//        System.out.println(vector.sub(scalar2));
+//        System.out.println(vector.div(scalar2));
+
+        System.out.println(vector.add(vector2));
+        System.out.println(vector.mul(vector2));
+//        System.out.println(vector.sub(vector2));
+//        System.out.println(vector.div(vector2));
+
+        System.out.println(scalar.add(vector2));
+        System.out.println(scalar.mul(vector2));
+//        System.out.println(scalar.sub(vector2));
+//        System.out.println(scalar.div(vector2));
+
+
+
+    }
+}
